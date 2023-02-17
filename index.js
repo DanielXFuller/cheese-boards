d5222d0db71f9f8e4717176aeb2965325c6ebbda
DanielXFuller <danielxfuller@gmail.com>
2023-02-17 12:14:16 +0000

    associations for the models

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const sequelize = require('./db');
 const User = require('./models/user');
 const Board = require('./models/board');
 const Cheese = require('./models/cheese');
+
+User.hasMany(Board);
+Board.belongsTo(User);
+Board.belongsToMany(Cheese, { through: 'BoardCheese' });
+Cheese.belongsToMany(Board, { through: 'BoardCheese' });
